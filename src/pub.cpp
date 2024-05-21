a83fbfa0dfef84df06baeb0f192d83007640554a
wu-sung <119473043+wu-sung@users.noreply.github.com>
2024-05-21 16:50:40 +0900

    Update pub.cpp

--- src/pub.cpp
+++ src/pub.cpp
@@ -1,24 +1,23 @@
-#include "rclcpp/rclcpp.hpp"  // ROS 2의 RCLCPP 라이브러리를 포함
-#include "sensor_msgs/msg/compressed_image.hpp"  // CompressedImage 메시지를 사용하기 위해 포함
-#include "cv_bridge/cv_bridge.h"  // OpenCV와 ROS 메시지 간 변환을 위해 포함
-#include "opencv2/opencv.hpp"  // OpenCV 라이브러리를 포함
-#include <memory>
-#include <chrono>
+#include "rclcpp/rclcpp.hpp"  // ROS 2의 RCLCPP 라이브러리 포함
+#include "sensor_msgs/msg/compressed_image.hpp"  // 압축된 이미지 메시지 타입 포함
+#include "cv_bridge/cv_bridge.h"  // OpenCV와 ROS 메시지 간 변환을 위한 브릿지 포함
+#include "opencv2/opencv.hpp"  // OpenCV 라이브러리 포함
+#include <memory>  // std::shared_ptr 사용을 위한 헤더 포함
+#include <chrono>  // 시간 관련 라이브러리 포함
 
 // GStreamer 파이프라인 문자열
 std::string src = "nvarguscamerasrc sensor-id=0 ! \
-	video/x-raw(memory:NVMM), width=(int)640, height=(int)360, \
+    video/x-raw(memory:NVMM), width=(int)640, height=(int)360, \
     format=(string)NV12 ! nvvidconv flip-method=0 ! video/x-raw, \
     width=(int)640, height=(int)360, format=(string)BGRx ! \
-	videoconvert ! video/x-raw, format=(string)BGR ! appsink"; 
+    videoconvert ! video/x-raw, format=(string)BGR ! appsink"; 
 
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);  // ROS 2 초기화
     auto node = std::make_shared<rclcpp::Node>("campub");  // 새로운 노드 생성
     auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort();  // QoS 설정
-    auto mypub = node->create_publisher<sensor_msgs::msg::CompressedImage>("image/compressed", qos_profile );
-    // 퍼블리셔 생성(메세지 형태)
+    auto mypub = node->create_publisher<sensor_msgs::msg::CompressedImage>("image/compressed", qos_profile);  // 퍼블리셔 생성
     
     std_msgs::msg::Header hdr;  // 메시지 헤더 생성
     sensor_msgs::msg::CompressedImage::SharedPtr msg;  // CompressedImage 메시지 포인터 생성
@@ -32,7 +31,7 @@ int main(int argc, char * argv[])
     }
     cv::Mat frame;  // 프레임을 저장할 변수
 
-    while(rclcpp::ok())  // ROS 2가 실행 중일 때
+    while (rclcpp::ok())  // ROS 2가 실행 중일 때
     {
         cap >> frame;  // 비디오 캡처에서 프레임을 읽기
         if (frame.empty()) {  // 프레임이 비어 있으면
@@ -44,5 +43,5 @@ int main(int argc, char * argv[])
         loop_rate.sleep();  // 루프 속도 유지
     }
     rclcpp::shutdown();  // ROS 2 종료
-    return 0; //0값을 리턴
+    return 0;  // 프로그램 종료
 }
